c6a01d01866929bbe80fa79bd2137368b71f0230
sokunthira <serverthira0826@gmail.com>
2020-06-10 23:42:04 +0700

    Initial commit

---
+++ client/main.js
@@ -0,0 +1,13 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+//import html
+import './main.html';
+import './html/login.html';
+import './html/tapbar.html';
+//import CSS
+import './css/main.css';
+
+
+//import JS
+import '../lib/routes/ironRouter.js';
